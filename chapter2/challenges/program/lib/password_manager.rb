8cd00ddf398dfe4a11237eb188549c00f0afb9ba
Zac <64330632+Garton3114@users.noreply.github.com>
2022-12-07 11:52:48 +0000

    ready for review

--- chapter2/challenges/program/lib/password_manager.rb
+++ chapter2/challenges/program/lib/password_manager.rb
@@ -1,2 +1,20 @@
-class PasswordManager     
+class PasswordManager    
+    def initialize
+        @password_manager = {
+        }
+    end
+    
+    def add(service, password)
+        if (password.length >= 8) && (password =~ (/[!@$%&]/))
+        @password_manager[service] = password
+        end
+    end
+    
+    def password_for(service)
+        @password_manager.fetch(service)
+    end
+    
+    def services
+        @password_manager.keys
+    end
 end
